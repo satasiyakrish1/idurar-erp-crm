984e7ae9b21f4732ff3ab47df9ed9e279937adf2
satasiyakrish1 <krishsatasiya44@gmail.com>
2026-02-24 11:08:18 +0530

    issue solved : fix(auth): resolve silent redirection bug after password reset

--- frontend/src/redux/auth/actions.js
+++ frontend/src/redux/auth/actions.js
@@ -4,106 +4,106 @@ import { request } from '@/request';
 
 export const login =
   ({ loginData }) =>
-  async (dispatch) => {
-    dispatch({
-      type: actionTypes.REQUEST_LOADING,
-    });
-    const data = await authService.login({ loginData });
+    async (dispatch) => {
+      dispatch({
+        type: actionTypes.REQUEST_LOADING,
+      });
+      const data = await authService.login({ loginData });
 
-    if (data.success === true) {
-      const auth_state = {
-        current: data.result,
-        isLoggedIn: true,
-        isLoading: false,
-        isSuccess: true,
-      };
-      window.localStorage.setItem('auth', JSON.stringify(auth_state));
-      window.localStorage.removeItem('isLogout');
-      dispatch({
-        type: actionTypes.REQUEST_SUCCESS,
-        payload: data.result,
-      });
-    } else {
-      dispatch({
-        type: actionTypes.REQUEST_FAILED,
-      });
-    }
-  };
+      if (data.success === true) {
+        const auth_state = {
+          current: data.result,
+          isLoggedIn: true,
+          isLoading: false,
+          isSuccess: true,
+        };
+        window.localStorage.setItem('auth', JSON.stringify(auth_state));
+        window.localStorage.removeItem('isLogout');
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          payload: data.result,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.REQUEST_FAILED,
+        });
+      }
+    };
 
 export const register =
   ({ registerData }) =>
-  async (dispatch) => {
-    dispatch({
-      type: actionTypes.REQUEST_LOADING,
-    });
-    const data = await authService.register({ registerData });
+    async (dispatch) => {
+      dispatch({
+        type: actionTypes.REQUEST_LOADING,
+      });
+      const data = await authService.register({ registerData });
 
-    if (data.success === true) {
-      dispatch({
-        type: actionTypes.REGISTER_SUCCESS,
-      });
-    } else {
-      dispatch({
-        type: actionTypes.REQUEST_FAILED,
-      });
-    }
-  };
+      if (data.success === true) {
+        dispatch({
+          type: actionTypes.REGISTER_SUCCESS,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.REQUEST_FAILED,
+        });
+      }
+    };
 
 export const verify =
   ({ userId, emailToken }) =>
-  async (dispatch) => {
-    dispatch({
-      type: actionTypes.REQUEST_LOADING,
-    });
-    const data = await authService.verify({ userId, emailToken });
+    async (dispatch) => {
+      dispatch({
+        type: actionTypes.REQUEST_LOADING,
+      });
+      const data = await authService.verify({ userId, emailToken });
 
-    if (data.success === true) {
-      const auth_state = {
-        current: data.result,
-        isLoggedIn: true,
-        isLoading: false,
-        isSuccess: true,
-      };
-      window.localStorage.setItem('auth', JSON.stringify(auth_state));
-      window.localStorage.removeItem('isLogout');
-      dispatch({
-        type: actionTypes.REQUEST_SUCCESS,
-        payload: data.result,
-      });
-    } else {
-      dispatch({
-        type: actionTypes.REQUEST_FAILED,
-      });
-    }
-  };
+      if (data.success === true) {
+        const auth_state = {
+          current: data.result,
+          isLoggedIn: true,
+          isLoading: false,
+          isSuccess: true,
+        };
+        window.localStorage.setItem('auth', JSON.stringify(auth_state));
+        window.localStorage.removeItem('isLogout');
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          payload: data.result,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.REQUEST_FAILED,
+        });
+      }
+    };
 
 export const resetPassword =
   ({ resetPasswordData }) =>
-  async (dispatch) => {
-    dispatch({
-      type: actionTypes.REQUEST_LOADING,
-    });
-    const data = await authService.resetPassword({ resetPasswordData });
+    async (dispatch) => {
+      dispatch({
+        type: actionTypes.REQUEST_LOADING,
+      });
+      const data = await authService.resetPassword({ resetPasswordData });
 
-    if (data.success === true) {
-      const auth_state = {
-        current: data.result,
-        isLoggedIn: true,
-        isLoading: false,
-        isSuccess: true,
-      };
-      window.localStorage.setItem('auth', JSON.stringify(auth_state));
-      window.localStorage.removeItem('isLogout');
-      dispatch({
-        type: actionTypes.REQUEST_SUCCESS,
-        payload: data.result,
-      });
-    } else {
-      dispatch({
-        type: actionTypes.REQUEST_FAILED,
-      });
-    }
-  };
+      if (data.success === true) {
+        const auth_state = {
+          current: data.result,
+          isLoggedIn: true,
+          isLoading: false,
+          isSuccess: true,
+        };
+        window.localStorage.setItem('auth', JSON.stringify(auth_state));
+        window.localStorage.removeItem('isLogout');
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          payload: data.result,
+        });
+      } else {
+        dispatch({
+          type: actionTypes.REQUEST_FAILED,
+        });
+      }
+    };
 
 export const logout = () => async (dispatch) => {
   dispatch({
@@ -138,20 +138,20 @@ export const logout = () => async (dispatch) => {
 
 export const updateProfile =
   ({ entity, jsonData }) =>
-  async (dispatch) => {
-    let data = await request.updateAndUpload({ entity, id: '', jsonData });
+    async (dispatch) => {
+      let data = await request.updateAndUpload({ entity, id: '', jsonData });
 
-    if (data.success === true) {
-      dispatch({
-        type: actionTypes.REQUEST_SUCCESS,
-        payload: data.result,
-      });
-      const auth_state = {
-        current: data.result,
-        isLoggedIn: true,
-        isLoading: false,
-        isSuccess: true,
-      };
-      window.localStorage.setItem('auth', JSON.stringify(auth_state));
-    }
-  };
+      if (data.success === true) {
+        dispatch({
+          type: actionTypes.REQUEST_SUCCESS,
+          payload: data.result,
+        });
+        const auth_state = {
+          current: data.result,
+          isLoggedIn: true,
+          isLoading: false,
+          isSuccess: true,
+        };
+        window.localStorage.setItem('auth', JSON.stringify(auth_state));
+      }
+    };
